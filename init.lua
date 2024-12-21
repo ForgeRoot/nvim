7c665c225b64dab3e8893fb6a518568762669a75
ForgeRoot <dylananne.v.eck@gmail.com>
2024-12-21 14:10:56 +0100

    initial commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+vim.opt.number = true		-- Show line numbers
+vim.opt.relativenumber = true	-- Show relative line numbers
+vim.opt.expandtab = true	-- Use spaces instead of tabs
+vim.opt.shiftwidth = 2		-- Number of spaces to use for auto-indent
+vim.opt.smartindent = true	-- Auto-indent based on previous line
+vim.opt.wrap = false		-- Don't wrap lines
